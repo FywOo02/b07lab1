40501cbad4d7518f86af988103af31f568b8506b
FywOo02 <zhushenzhe0531@gmail.com>
2023-09-11 20:53:25 -0400

    added Polynomial.java and Driver.java

---
+++ Polynomial.java
@@ -0,0 +1,45 @@
+public class Polynomial {
+    double[] coe;
+
+    public Polynomial(){
+        this.coe = new double[]{0};
+    }
+
+    public Polynomial(double[] coe) {
+        this.coe = coe;
+    }
+
+    public Polynomial add(Polynomial input){
+        double[] result = new double[this.coe.length+input.coe.length];
+
+        int count = 0;
+        int index = 0;
+        while(count < this.coe.length){
+            result[index] = this.coe[count];
+            index++;
+            count++;
+        }
+        count = 0;
+        while (count < input.coe.length){
+            result[index] = input.coe[count];
+            index++;
+            count++;
+        }
+        return new Polynomial(result);
+    }
+
+    public double evaluate(double x){
+        double result = 0;
+        for(int count = 0; count < this.coe.length; count++){
+            result += this.coe[count] * Math.pow(x,count);
+        }
+        return result;
+    }
+
+    public boolean hasRoot(double num){
+        if (evaluate(num) == 0)
+            return true;
+        else
+            return false;
+    }
+}
